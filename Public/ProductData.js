2e3d45b7f01b38a70e4a55b7e31f74c51eb2a1bf
Shaira25 <59968971+Shaira25@users.noreply.github.com>
2020-04-29 01:45:27 -1000

    Add files via upload

---
+++ Public/ProductData.js
@@ -0,0 +1,46 @@
+products =
+[
+    {
+        // Product 1
+        "Facemask": "Classic",
+        "price": 10,
+        "price": 7,
+        "image": "./images/1.jpg"
+    },
+    {
+        // Product 2
+        "Facemask": "Aloha",
+        "price": 15,
+        "image": "./images/2.jpg"
+    },
+    {
+        // Product 3
+        "Facemask": "Shy Gray",
+        "price": 15,
+        "image": "./images/3.jpg"
+    },
+    {
+        // Product 4
+        "Facemask": "Travel Blue",
+        "price": 15,
+        "image": "./images/4.jpg"
+    },
+    {
+        // Product 5
+        "Facemask": "Baby Feels",
+        "price": 15,
+        "image": "./images/5.jpg"
+    },
+    {
+        // Product 6
+        "Facemask": "Dark Flower",
+        "price": 15,
+        "image": "./images/6.jpg"
+    }
+];
+
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
+
+
