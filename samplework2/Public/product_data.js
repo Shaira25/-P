acfc9bf82161d34c2c13e82e9e1c67172aa62257
Shaira25 <59968971+Shaira25@users.noreply.github.com>
2020-06-20 15:02:44 +0900

    Add files via upload

---
+++ samplework2/Public/product_data.js
@@ -0,0 +1,45 @@
+//Assignment 3 by Shane Gimeenez and Marc Masaoay//
+//codes are from lab 13 exercise 3//
+//Images are own by me (Shane Gimenez)//
+
+var productNames = ["classic_products", "aloha_products", "shy_products", "travel_products"];
+
+var products = {
+
+"classic_products" :
+    {
+        // Product 1
+        "Facemask": "Classic",
+        "price": 10,
+        "price": 7,
+        "image": "./images/1.jpg"
+    },
+"aloha_products" :
+    {
+        // Product 2
+        "Facemask": "Aloha",
+        "price": 15,
+        "image": "./images/2.jpg"
+    },
+"shy_products" :
+    {
+        // Product 3
+        "Facemask": "Shy Gray",
+        "price": 15,
+        "image": "./images/3.jpg"
+    },
+"travel_products" :
+    {
+        // Product 4
+        "Facemask": "Travel Blue",
+        "price": 15,
+        "image": "./images/4.jpg"
+    },
+};
+
+exports.products = products;
+exports.productNames = productNames;
+
+
+
+
